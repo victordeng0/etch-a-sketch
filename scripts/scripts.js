a19d877380a435802d10e1f317484c49836a1aaa
victordeng0 <victordeng8@gmail.com>
2020-06-22 03:35:20 -0400

    created grid using CSS grid; used loop and selectorAll with [i] to add event listener

---
+++ scripts/scripts.js
@@ -0,0 +1,44 @@
+
+
+
+for (let i = 0; i < 256; i++) {
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+//content.textContent = 'Hello World';
+
+container.appendChild(content);
+}
+
+// for (let i = 0; i<16; i++) {
+//     const row = document.querySelector('.content');
+
+//     const nested = document.createElement('div');
+//     nested.classList.add('nested');
+
+//     nested.textContent = 'Hello World';
+//     row.appendChild('nested');
+// }
+
+ const colorChange = document.querySelectorAll('.content');
+
+ for (let i = 0; i<colorChange.length; i++) {
+     colorChange[i].addEventListener('mouseover', () => {
+        colorChange[i].classList.add('black');
+     
+     })
+ }
+
+//  colorChange.addEventListener('mouseover', () => {
+
+//     colorChange.classList.add('black')
+   
+
+//  });
+
+ const btn = document.querySelector('button');
+
+ btn.addEventListener('click', () => {
+     colorChange.classList.remove('black');
+ })
